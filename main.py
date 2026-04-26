14c35f8b8319875d76ff1948af911199e84369f2
Jenisha <jenishereyl@gmail.com>
2026-04-26 12:44:07 +0530

    feat: add data validation component to training pipeline

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import sys
 from networksecurity.components.data_ingestion import DataIngestion
+from networksecurity.components.data_validation import DataValidation
 from networksecurity.exception.exception import NetworkSecurityException
 from networksecurity.logging.logger import logging
-from networksecurity.entity.config_entity import DataIngestionConfig
+from networksecurity.entity.config_entity import DataIngestionConfig, DataValidationConfig
 from networksecurity.entity.config_entity import TrainingPipelineConfig
 if __name__ == '__main__':
     try:
@@ -11,7 +12,15 @@ if __name__ == '__main__':
         data_ingestion = DataIngestion(data_ingestion_config)
         logging.info("Initiate the data ingestion")
         data_ingestion_artifact = data_ingestion.initiate_data_ingestion()
+        logging.info("Data Initiation Completed")
         print(data_ingestion_artifact)
+        data_validation_config =  DataValidationConfig(training_pipeline_config)
+        data_validation = DataValidation(data_ingestion_artifact, data_validation_config)
+        logging.info("Initiate data validation")
+        data_validation_artifact = data_validation.initiate_data_validation()
+        logging.info("Data validation completed")
+        print(data_validation_artifact)
+        \
 
     except Exception as e:
         raise NetworkSecurityException(e, sys)
